a11d6b52909ae8e25a95eea752fe67530d83156d
Vici Shaweddy <vshaweddy@gmail.com>
2020-01-21 12:51:12 -0500

    added func for permission

--- VideoRecorder/VideoRecorder/ViewController.swift
+++ VideoRecorder/VideoRecorder/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 	
@@ -15,14 +16,43 @@ class ViewController: UIViewController {
 		// Do any additional setup after loading the view.
 	}
 	
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
-		
-		// TODO: get permission
-		
-		showCamera()
-		
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+        requestPermissionAndShowCamera()
 	}
+    
+    private func requestPermissionAndShowCamera() {
+        let status = AVCaptureDevice.authorizationStatus(for: .video)
+        
+        switch status {
+        case .notDetermined:
+            // First time user has seen the dialog, we don't have permission
+            requestPermission()
+        case .restricted:
+            // parental controls
+            fatalError("Video is diabled for parental controls")
+        case .denied:
+            // we asked for permission and they said no
+            fatalError("Tell user they need to enable Privacy for Video/Camera/Microphone")
+        case .authorized:
+            // we asked for permission and they said yes
+            showCamera()
+        default:
+            fatalError("A new status was added that we need to handle")
+        }
+    }
+    
+    private func requestPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { (granted) in
+            guard granted else {
+                fatalError("Tell user they need to enable Privacy for Video/Camera/Microphone")
+            }
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.showCamera()
+            }
+        }
+    }
 	
 	private func showCamera() {
 		performSegue(withIdentifier: "ShowCamera", sender: self)
